ff697534c2f31955f6f4af7c78c6a63bbdf10312
phiresky <phireskyde+git@gmail.com>
2018-07-16 12:26:20 +0200

    cleanup vscode plugin code a bit

--- vscode-deep-complete/src/extension.ts
+++ vscode-deep-complete/src/extension.ts
@@ -1,18 +1,13 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
-"use strict";
-
 import * as vscode from "vscode";
 import { spawn, ChildProcess } from "child_process";
 import * as readline from "readline";
 import { Readable } from "stream";
 
+/** simple node stream -> Promises wrapper */
 class LineReader {
 	private reader: readline.ReadLine;
-	private _res: (p: string) => void;
-	private _prom: Promise<string>;
+	private _res!: (p: string) => void;
+	private _prom!: Promise<string>;
 	constructor(readable: Readable) {
 		this.reader = readline.createInterface(proc.stdout);
 		this.genProm();
@@ -28,17 +23,19 @@ class LineReader {
 		});
 	}
 }
-type deepConf = {
-	n: number;
-	mode: 0 | 1 | 2;
-	prime: string;
-};
-type deepRes = {
-	result: string;
-};
-type deepConfP = Pick<deepConf, "n" | "mode">;
+/** type of the json object sent to the python process */
+namespace IPC {
+	export type Out = {
+		n: number;
+		mode: 0 | 1 | 2;
+		prime: string;
+	};
+	export type In = {
+		result: string;
+	};
+}
 
-type deepArg = "line" | "eof" | { chars: number };
+type DeepArg = "line" | "stepwise" | "eof" | { chars: number };
 
 let proc: ChildProcess;
 export function activate(context: vscode.ExtensionContext) {
@@ -50,18 +47,14 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	proc.stderr.on("data", chunk => console.error("E:", chunk.toString()));
 	proc.stdout.on("data", chunk => console.log("O:", chunk.toString()));
-	async function sendCommand(p: deepConf): Promise<deepRes> {
+	async function sendCommand(p: IPC.Out): Promise<IPC.In> {
 		if (p.prime === "") p.prime = "e";
 		const i = JSON.stringify(p) + "\n";
 		console.log("I", i);
-		proc.stdin.write(i, err => err && console.error("WRI", err));
+		proc.stdin.write(i, (err: any) => err && console.error("WRI", err));
 		return JSON.parse(await reader.next());
 	}
 	const reader = new LineReader(proc.stdout);
-	// The most simple completion item provider which
-	// * registers for text files (`'plaintext'`), and
-	// * return the 'Hello World' and
-	//   a snippet-based completion item.
 	vscode.languages.registerCompletionItemProvider(["typescript", "typescriptreact"], {
 		provideCompletionItems(
 			document: vscode.TextDocument,
@@ -80,8 +73,7 @@ export function activate(context: vscode.ExtensionContext) {
 				command: "deep.complete",
 				arguments: [{ chars: 1000 }]
 			};
-			a.insertText = "";
-			a.sortText = "ZZ";
+			a.insertText = ""; // text will be inserted asynchronously by command
 
 			let b = new vscode.CompletionItem("¯\\_(ツ)_/¯ Deep complete line", vscode.CompletionItemKind.Snippet);
 			b.filterText = "uhhh";
@@ -100,36 +92,16 @@ export function activate(context: vscode.ExtensionContext) {
 			c.command = {
 				title: "deep complete",
 				command: "deep.complete",
-				arguments: ["eof"]
+				arguments: ["stepwise"]
 			};
 			c.insertText = "";
 			return [a, b, c];
 		}
-		/*async resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken) {
-			//console.log(item.insertText, item);
-			if (item.insertText === "[processing]") {
-				proc.stdin.write(JSON.stringify({ n: 100, prime: "hello", mode: 1 }) + "\n", (err, res) => {
-					if (err) console.error("WRI", err);
-				});
-				const line = JSON.parse(await reader.next());
-				console.log("GOT", line);
-				const c = new vscode.CompletionItem(item.label);
-				c.insertText = line.result || "EMPTY";
-				return c;
-			} else {
-				console.log("HEW");
-				const document = (item as any)._doc as vscode.TextDocument;
-				const position = (item as any)._pos as vscode.Position;
-				console.log("RANG", document.getText(document.getWordRangeAtPosition(position)));
-				return new vscode.CompletionItem(item.label + "[]" + Math.random());
-			}
-			//console.log(select);
-			//return Promise.resolve(new vscode.CompletionItem("YNOOOO"));
-		}*/
 	});
-	async function doThing(d: deepArg) {
+	async function doCompleteCommand(d: DeepArg) {
 		try {
 			let editor = vscode.window.activeTextEditor;
+			if(!editor) return;
 			const pos = editor.selection.active;
 			console.log("RUN");
 			const prime = editor.document
@@ -142,15 +114,16 @@ export function activate(context: vscode.ExtensionContext) {
 				const res = await sendCommand({ prime, mode: 2, n: 200 });
 				resText = (res.result || "[EMTPYRES]").slice(primeLength);
 				resText = resText.slice(0, resText.indexOf("\n"));
-			} else if (d === "eof") {
+			} else if (d === "stepwise") {
 				for (let i = 0; i < 20; i++) {
-					await doThing({ chars: 80 });
+					await doCompleteCommand({ chars: 80 });
 					await new Promise(res => setTimeout(res, 100));
 				}
 				return;
-				/*const res = await sendCommand({ prime, mode: 2, n: 5000 });
+			} else if (d === "eof") {
+				const res = await sendCommand({ prime, mode: 2, n: 5000 });
 				resText = (res.result || "[EMTPYRES]").slice(primeLength);
-				resText = resText.slice(0, resText.indexOf("// [EOF]"));*/
+				resText = resText.slice(0, resText.indexOf("// [EOF]"));
 			} else {
 				const res = await sendCommand({ prime, mode: 2, n: d.chars });
 				resText = (res.result || "[EMTPYRES]").slice(primeLength);
@@ -158,14 +131,18 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			console.log("RESTE", resText);
 			editor = vscode.window.activeTextEditor;
+			if(!editor) {
+				console.error("editor closed");
+				return;
+			}
 			editor.edit(e => {
-				e.insert(editor.selection.active, resText);
+				e.insert(editor!.selection.active, resText);
 			});
 		} catch (e) {
 			console.error("CMOP", e);
 		}
 	}
-	vscode.commands.registerCommand("deep.complete", doThing);
+	vscode.commands.registerCommand("deep.complete", doCompleteCommand);
 }
 export function deactivate() {
 	console.log("killing external");
